f647ef879e93d4b0b549725ed2950e1d2b09aa5c
RostenRoss <rosten2017@gmail.com>
2019-03-21 18:50:32 +0100

    third Commit

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express();
 
 //replace the '/dist/<yo_your_prohect_name'
 
-app.use(express.static( __dirname + '/dist/rosten-ross'));
+app.use(express.static( __dirname + '/rosten-ross'));
 
 app.get('*', function(req, res){
   //replace the '/dist/<to_your_proyect_name>/index.html'
 
-  res.sendFile(path.join( __dirname + '/dist/rosten-ross/index.html'));
+  res.sendFile(path.join( __dirname + '/rosten-ross/index.html'));
 });
 //Start the app by listening on the default Heroku port
 
