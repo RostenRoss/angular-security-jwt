00593e45147304a5cfb270d81e200df4cb513bab
RostenRoss <rosten2017@gmail.com>
2019-03-22 01:23:06 +0100

    third Commit

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/rosten-ross'));
 
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/rosten-ross/index.html');
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/rosten-ross/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
