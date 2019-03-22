be157afaf97885b7b4639306e265ee9bcb122abd
RostenRoss <rosten2017@gmail.com>
2019-03-22 01:19:41 +0100

    third Commit

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 //Install express server
-const express =require('express');
-
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
-//server only the static files form the dist directory
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/rosten-ross'));
 
-//replace the '/dist/<yo_your_prohect_name'
-
-app.use(express.static( __dirname + '/dist/rosten-ross'));
-
-app.get('*', function(req, res){
-  //replace the '/dist/<to_your_proyect_name>/index.html'
-
-  res.sendFile(path.join( __dirname + '/dist/rosten-ross/index.html'));
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/rosten-ross/index.html');
 });
-//Start the app by listening on the default Heroku port
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
